eb5b0bafee82d9c758d67bd434282371940f68ed
Kevin Li <kevin.li20021106@gmail.com>
2022-08-06 22:43:18 -0400

    Finish Register and Login

--- web/static/web/app.js
+++ web/static/web/app.js
@@ -11,3 +11,24 @@ if (document.querySelector("nav")) {
     });
   });
 }
+
+// Sign Form Button Enable/Disable
+if (document.querySelector(".sign-form")) {
+  const button = document.querySelector(".disabled");
+  const all_input = document.querySelectorAll("input");
+  all_input.forEach((input) => {
+    input.oninput = () => {
+      let all_filled = true;
+      all_input.forEach((ipt) => {
+        if (ipt.value === "") {
+          all_filled = false;
+        }
+      });
+      if (all_filled) {
+        button.classList.remove("disabled");
+      } else {
+        button.classList.add("disabled");
+      }
+    };
+  });
+}
