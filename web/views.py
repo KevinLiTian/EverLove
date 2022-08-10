517ee8ec9c7ca35ce2d5b4ffd112e7af276fb566
Kevin Li <kevin.li20021106@gmail.com>
2022-08-10 19:06:07 -0400

    Update views.py

--- web/views.py
+++ web/views.py
@@ -64,7 +64,8 @@ def register(request):
 
         # Attempt to create new user
         try:
-            user = User.objects.create_user(username, password)
+            user = User.objects.create_user(username=username,
+                                            password=password)
             user.save()
         except IntegrityError:
             return render(request, "web/register.html",
